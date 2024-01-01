763c6011931f7023e811cd541d328fd2b1081cdb
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2024-01-01 17:04:36 +0000

    Refactor Programs component and page.tsx
    The commit message suggests that the developer has refactored the Programs component and page.tsx file.

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import programsData from '@/data/programs.json'
-import Programs from '@/components/programs/Programs'
-
-const page = ({params , searchParams}: any) => {
-
-  const search = searchParams.search || "";
+import Programs from "@/components/programs/Programs";
+import  programsData  from "@/data/programs.json";
 
 
+const page = ({ params, searchParams }: any) => {
+
+  const search = searchParams?.search || "";
   return (
-    <div>page
+    <div>
+      
 
-    <Programs programs={programsData.filter((prg)=> prg.code.includes(search) || prg.name.includes(search)).slice(0,12) as any} />
+      <Programs
+        programs={
+          programsData
+            .filter(
+              (prg) => prg.code.includes(search) || prg.name.includes(search)
+            )
+            .slice(0, 12) as any
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
