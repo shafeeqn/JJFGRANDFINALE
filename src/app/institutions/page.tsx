063b331b923af133e7ae30f0184321b7c15943fd
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2024-01-02 09:02:00 +0000

    Add search functionality and display institutions

---
+++ src/app/institutions/page.tsx
@@ -0,0 +1,26 @@
+import Institution from "@/components/institutions/Institution";
+import institutionsData from "@/data/institutions.json";
+
+const page = ({ params, searchParams }: any) => {
+  const search = searchParams?.search || "";
+  return (
+    <div>
+      <Institution
+        institutions={
+          institutionsData
+            .filter(
+              (instn) =>
+                instn.code
+                  .toString()
+                  .toLowerCase()
+                  .includes(search.toLowerCase()) ||
+                instn.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .slice(0, 12) as any
+        }
+      />
+    </div>
+  );
+};
+
+export default page;
