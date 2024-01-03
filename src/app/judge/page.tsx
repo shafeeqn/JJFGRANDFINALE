d1cff8b883770f2d8f18f727209845c22fe19654
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-01-04 03:00:51 +0530

    Refactor code to improve performance and readability

---
+++ src/app/judge/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Judge from '@/components/programs/Judge'
+
+const page = () => {
+
+  return (
+    <div>
+        <Judge />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/programs/Judge.tsx
@@ -0,0 +1,9 @@
+const Judge = () => {
+    return (
+        <div>
+        <h1>Judge</h1>
+        </div>
+    )
+}
+
+export default Judge
