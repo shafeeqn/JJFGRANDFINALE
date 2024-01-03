cb30d9e8c76319de458b223e23a199574b2e9e3c
Your GitHub Username <your.email@example.com>
2024-01-03 22:07:41 +0530

    changes in data

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import candidatesData from '@/data/candidates.json'
+import programsData from "@/data/programs.json"
+import darsData from '@/data/institutions.json'
+import cp from '@/data/cp.json'
 
 const page = () => {
+  const publishedPrograms = programsData.filter((prg)=>prg.publish != 1 )
+  const candidateWithProgram = publishedPrograms
   return (
     <div>page</div>
   )
