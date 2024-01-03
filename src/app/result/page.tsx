652afe6bf3d1682898c0674468499d4aaa53694b
Your GitHub Username <your.email@example.com>
2024-01-03 23:13:35 +0530

    Refactor candidateWithProgram and darsWithTotalPoints calculations

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -6,7 +6,40 @@ import cp from '@/data/cp.json'
 
 const page = () => {
   const publishedPrograms = programsData.filter((prg)=>prg.publish != 1 )
-  const candidateWithProgram = publishedPrograms
+
+  const candidateWithProgram = publishedPrograms.map((prg)=>{
+    const candidate = cp.filter((c)=> c.prg == prg.code)
+    return {
+      ...prg,
+      candidate: candidate.map((cnd)=> candidatesData.find((c)=> {
+        if(c.chest == cnd.code){
+          return {
+            ...c,
+            cp: cnd
+          }
+        }
+      }))
+    }
+  })
+
+  const darsWithTotalPoints = darsData.map((dars)=>{
+    let totalPoints = 0;
+
+    candidateWithProgram.map((prg)=>{
+      prg.candidate.map((cnd)=>{
+        if(cnd?.dars == dars.name){
+          totalPoints += cnd.pts
+        }
+      })
+    })
+
+    return {
+      ...dars,
+      totalPoints
+    }
+  })
+  
+  
   return (
     <div>page</div>
   )
