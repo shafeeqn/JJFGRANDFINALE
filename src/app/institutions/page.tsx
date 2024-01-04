59d6d7d57744da8470e4865f57d5c4b65dbd2451
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-01-04 13:38:20 +0530

    Fix candidateWithProgram typo and uncomment code

--- src/app/institutions/page.tsx
+++ src/app/institutions/page.tsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 import Institution from "@/components/institutions/Institution";
 import institutionsData from "@/data/institutions.json";
 import candidatesData from "@/data/candidates.json";
@@ -8,19 +8,19 @@ import candidateProgramsData from "@/data/cp.json";
 const page = ({ params, searchParams }: any) => {
   const search = searchParams?.search || "";
 
-  // const candidateWithProgram: any = candidatesData.map((cnd) => {
-  //   const cps = candidateProgramsData.filter((cp) => cp.code == cnd.chest);
-  //   return {
-  //     ...cnd,
-  //     propgrams : cps.map((cp)=>{
-  //       return {
-  //         ...programsData.find((prg)=> prg.code == cp.prg)
-  //       }
-  //     })
-  //   };
-  // });
+  const candidateWithProgram: any = candidatesData.map((cnd) => {
+    const cps = candidateProgramsData.filter((cp) => cp.code == cnd.chest);
+    return {
+      ...cnd,
+      propgrams : cps.map((cp)=>{
+        return {
+          ...programsData.find((prg)=> prg.code == cp.prg)
+        }
+      })
+    };
+  });
 
-  // console.log(candidateWithProgram);
+  console.log(candidateWithProgram);
 
   return (
     <div>
