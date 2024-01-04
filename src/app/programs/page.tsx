471e6ef19b52f270ff4319d787c5f409c12e5be6
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-01-04 23:23:05 +0530

    Update page.tsx

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -13,7 +13,7 @@ const page = ({ params, searchParams }: any) => {
                 prg.code.toLowerCase().includes(search.toLowerCase()) ||
                 prg.name.toLowerCase().includes(search.toLowerCase())
             )
-            .slice(0, 12) as any
+            .slice(0, 75) as any
         }
       />
     </div>
