bc363cbd0206ec8f8ddbc0036532c351e7264c22
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-11-27 22:59:55 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ function page() {
         Jamia Junior Fest
       </p>
       <p className="lg:text-lg font-black mb-5 text-center">
-Jamia Junior Colleges Arts Fest '25 (ZONE A)     </p>
+Jamia Junior Colleges Arts Fest '25     </p>
+      <p className="lg:text-lg font-black mb-5 text-center">
+ (ZONE A)     </p>
       <div className="flex gap-2 text-xl font-semibold">
         <Link href={'/result'}  className="py-1 bg-yellow border-yellow hover:bg-yellower border-2 px-2 rounded-md ">
           Results
