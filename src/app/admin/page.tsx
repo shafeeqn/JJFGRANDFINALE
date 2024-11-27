45e8a9af5c329253b5d8b44a18f4babeb7591028
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-11-27 22:40:52 +0530

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,7 +9,7 @@ function page() {
         Jamia Junior College Fest
       </p>
       <p className="lg:text-lg font-black mb-5 text-center">
-Grand Finale 2024     </p>
+Jamia Junior Colleges Arts Fest '25 (ZONE A)     </p>
       <div className="flex gap-2 text-xl font-semibold">
         <Link href={'/programs'}  className="py-1 bg-yellow border-yellow hover:bg-yellower border-2 px-2 rounded-md ">
           Programs
