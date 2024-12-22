4139ae4ee8234fc2cb0cc33575867bffef721d1a
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-12-22 13:18:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ function page() {
       <p className="lg:text-[72px] text-3xl font-black mb-5 text-center">
         Jamia Junior Fest
       </p>
-      <p className="lg:text-lg font-black mb-5 text-center">
+      <p className="lg:text-lg font-black color: #cc0000 mb-5 text-center">
         JAMIA JUNIOR COLLEGE ARTS FEST '25     </p>
       <p className="lg:text-[36px] font-black mb-5 text-center">
         (GRAND FINALE)     </p>
