ab506a1c87dd61c083ea0ddce9304b6e40f05491
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-12-25 17:54:34 +0530

    Update JdgList.tsx

--- src/components/programs/JdgList.tsx
+++ src/components/programs/JdgList.tsx
@@ -14,7 +14,7 @@ const JdgList = (props: Props) => {
         <div>
           <div className="text-center">
             <p className="text-3xl font-bold ">Jamia Junior College Fest &apos;25</p>
-            <p className="text-2xl font-bold my-2">GRAND FINALE</p>
+            <p className="print:bg-black print:text-white bg-black text-white>GRAND FINALE</p>
             <p className="text-2xl font-bold my-2">Judge Slip</p>
             <h1 className="font-bold mt-2 border-y-2 mb-4 border-black">
               {props.program.name} ({props.program.cat})
