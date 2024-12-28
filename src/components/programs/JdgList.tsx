b874c347f5c6368e2f517e1c2845fb29b7d19f21
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-12-28 15:16:42 +0530

    Update JdgList.tsx

--- src/components/programs/JdgList.tsx
+++ src/components/programs/JdgList.tsx
@@ -14,57 +14,7 @@ const JdgList = (props: Props) => {
         <div>
           <div className="text-center">
             <p className="text-2xl font-bold ">JAMIA JUNIOR FEST &apos;25</p>
-            <p className="text-2xl font-bold my-2">GRAND FINALE</p>
-            <p className="text-2xl font-bold my-2 bg-black text-white">Judge Slip</p>
-            <h1 className="font-bold mt-2 border-y-2 mb-4 border-black">
-             {props.program?.code}. {props.program.name} ({props.program.cat})
-            </h1>
-          </div>
-          <table className="w-full max-w-[700px] border border-slate-800">
-            <thead>
-              <tr className="print:bg-black print:text-white bg-black text-white">
-                <th className="p-1">Sl</th>
-                <th className="p-1">Chest</th>
-                <th className="p-1">Lott</th>
-                <th className="p-1">Mark</th>
-                <th className="p-1">Remark</th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.program.candidates.map((c: any, i: any) => (
-                <tr key={i}>
-                  <td className=" border py-4 border-slate-800 px-1 text-center w-8">
-                    {i + 1}
-                  </td>
-                  <td className=" border py-4 border-slate-800 px-1 text-center w-20">
-                    {c.code}
-                  </td>
-                  <td className=" border py-4 border-slate-800 px-1"></td>
-                  <td className=" border py-4 border-slate-800 px-1"></td>
-                  <td className=" border py-4 border-slate-800 px-1"></td>
-                </tr>
-              ))}
-              <tr>
-                <td className=" border py-4 border-slate-800 px-1">&nbsp;</td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-              </tr>
-              <tr>
-                <td className=" border py-4 border-slate-800 px-1">&nbsp;</td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-                <td className=" border py-4 border-slate-800 px-1"></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div>
-          <div className="text-center">
-            <p className="text-2xl font-bold ">JAMIA JUNIOR FEST &apos;25</p>
-            <p className="text-2xl font-bold my-2">GRAND FINALE</p>
+            <p className="text-xl font-bold my-2">GRAND FINALE</p>
             <p className="text-2xl font-bold my-2 bg-black text-white">Judge Slip</p>
             <h1 className="font-bold mt-2 border-y-2 mb-4 border-black">
              {props.program?.code}. {props.program.name} ({props.program.cat})
@@ -114,7 +64,57 @@ const JdgList = (props: Props) => {
         <div>
           <div className="text-center">
             <p className="text-2xl font-bold ">JAMIA JUNIOR FEST &apos;25</p>
-            <p className="text-2xl font-bold my-2">GRAND FINALE</p>
+            <p className="text-xl font-bold my-2">GRAND FINALE</p>
+            <p className="text-2xl font-bold my-2 bg-black text-white">Judge Slip</p>
+            <h1 className="font-bold mt-2 border-y-2 mb-4 border-black">
+             {props.program?.code}. {props.program.name} ({props.program.cat})
+            </h1>
+          </div>
+          <table className="w-full max-w-[700px] border border-slate-800">
+            <thead>
+              <tr className="print:bg-black print:text-white bg-black text-white">
+                <th className="p-1">Sl</th>
+                <th className="p-1">Chest</th>
+                <th className="p-1">Lott</th>
+                <th className="p-1">Mark</th>
+                <th className="p-1">Remark</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.program.candidates.map((c: any, i: any) => (
+                <tr key={i}>
+                  <td className=" border py-4 border-slate-800 px-1 text-center w-8">
+                    {i + 1}
+                  </td>
+                  <td className=" border py-4 border-slate-800 px-1 text-center font-bold w-20">
+                    {c.code}
+                  </td>
+                  <td className=" border py-4 border-slate-800 px-1"></td>
+                  <td className=" border py-4 border-slate-800 px-1"></td>
+                  <td className=" border py-4 border-slate-800 px-1"></td>
+                </tr>
+              ))}
+              <tr>
+                <td className=" border py-4 border-slate-800 px-1">&nbsp;</td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+              </tr>
+              <tr>
+                <td className=" border py-4 border-slate-800 px-1">&nbsp;</td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+                <td className=" border py-4 border-slate-800 px-1"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <div className="text-center">
+            <p className="text-2xl font-bold ">JAMIA JUNIOR FEST &apos;25</p>
+            <p className="text-xl font-bold my-2">GRAND FINALE</p>
             <p className="text-2xl font-bold my-2 bg-black text-white">Judge Slip</p>
 
             <h1 className="font-bold mt-2 border-y-2 mb-4 border-black">
@@ -139,7 +139,7 @@ const JdgList = (props: Props) => {
                   <td className=" border py-4 border-slate-800 px-1 text-center w-8">
                     {i + 1}
                   </td>
-                  <td className=" border py-4 border-slate-800 px-1 text-center w-20">
+                  <td className=" border py-4 border-slate-800 px-1 text-center font-bold w-20">
                     {c.code}
                   </td>
                   <td className=" border py-4 border-slate-800 px-1"></td>
