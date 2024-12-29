6e7aaa618e2cf56217bcd9a28b293bacd05566b0
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-12-29 21:42:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ function page() {
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <img src="/jam.jpg" className="w-96 h-96 object-cover -my-20" />
       <p className="lg:text-[72px] text-3xl font-black text-red mb-5 text-center">
-        Jamia Junior Fest
+        JAMIA JUNIOR FEST
       </p>
       <p className="lg:text-lg font-black mb-5 text-center">
-        JAMIA JUNIOR COLLEGE ARTS FEST '25     </p>
-      <p className="lg:text-[36px] font-black text-red mb-5 text-center">
+        Jamia Junior College Arts Fest '25     </p>
+      <p className="lg:text-[36px] font-brown text-red mb-5 text-center">
         (GRAND FINALE)     </p>
       <div className="flex gap-2 text-xl font-semibold">
         <Link href={'/result'} className="py-1 bg-yellow border-yellow hover:bg-yellower border-2 px-2 rounded-md ">
